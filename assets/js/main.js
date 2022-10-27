03c6c4af92f4cff0bf6a56a87c7bb08f738ce70a
Max <mdiazn@uni.pe>
2022-10-27 18:06:22 -0500

    cambiar background Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,12 @@ let homeSwiper = new Swiper(".home-swiper", {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== NEW SWIPER ===============*/
 
